6e72adb49171d9e24bef7fa36ce0f2036a409a99
Kasper Janssens <kasper.janssens@tomtom.com>
2022-03-23 09:31:08 +0100

    Seems to work

--- start_db.sh
+++ start_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d -p 5432:5432 --name test-db-smds -e POSTGRES_USER=kasper -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=mds_prod postgres
+docker run -d -p 5432:5432 --name test-db-smds -e POSTGRES_USER=kasper -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=mds_prod postgis/postgis
 
